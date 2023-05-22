103cd72910ad6719b0e76b3a20502c9e4dcc0f97
Bala <mailtobalas@gmail.com>
2023-05-22 16:54:08 +0530

    test again

--- src/main/java/com/testcicd/testcide/TestcideApplication.java
+++ src/main/java/com/testcicd/testcide/TestcideApplication.java
@@ -9,7 +9,7 @@ public class TestcideApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestcideApplication.class, args);
 
-		System.out.println("test cicd bala test......");
+		System.out.println("test cicd bala test test......");
 	}
 
 }
