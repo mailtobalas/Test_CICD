86038c9e9d91ba5e609ba1abcd3e993b16bdaf80
Bala <mailtobalas@gmail.com>
2023-05-22 11:33:21 +0530

    added first time
    added first time

---
+++ src/main/java/com/testcicd/testcide/TestcideApplication.java
@@ -0,0 +1,15 @@
+package com.testcicd.testcide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcideApplication.class, args);
+
+		System.out.println("test cicd ......");
+	}
+
+}
---
+++ src/test/java/com/testcicd/testcide/TestcideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcicd.testcide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
