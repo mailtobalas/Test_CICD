7303723571e538709bff646c055add1389ac4ee1
mailtobalas <62017029+mailtobalas@users.noreply.github.com>
2023-05-22 16:53:18 +0530

    test again

--- src/main/java/com/testcicd/testcide/TestcideApplication.java
+++ src/main/java/com/testcicd/testcide/TestcideApplication.java
@@ -9,7 +9,7 @@ public class TestcideApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestcideApplication.class, args);
 
-		System.out.println("test cicd bala......");
+		System.out.println("test cicd bala test......");
 	}
 
 }
